764d277851661601a3ada6215adcdbe4707d1ed1
Nivetha Manikandan <34644928+NivethaM19@users.noreply.github.com>
2017-12-19 21:18:39 +0530

    Create variableass.c

---
+++ Beginner/variableass.c
@@ -0,0 +1,21 @@
+/*Declare variables for storing information about Sachin Tendulkar like Name,Nick Name,Runs scored in ODI,Batting average in ODI,ODI Matches
+
+Expected Output:
+
+Sachin Ramesh Tendulkar
+Little Master
+18426
+44.83
+463*/
+
+#include<stdio.h>
+int main(void)
+{
+char name[30]="Sachin Ramesh Tendulkar';
+char nick[25]="Little Master";
+int odi=18426;
+float avg=44.86;
+int match=463;
+printf("%c\n%c\n%d\n%f\n%d",name,nick,odi,avg,match);
+getch();
+}
