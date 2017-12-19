ed7c93b97af7a09a046790fec4a339a5f12624d3
Nivetha Manikandan <34644928+NivethaM19@users.noreply.github.com>
2017-12-19 21:39:37 +0530

    Create scopeass.c

---
+++ Player/scopeass.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int count=0;
+int main(void)
+{
+int count=0;
+do
+{
+  int count=0;
+  ++count;
+  printf("\n%d",count);
+}while(++count<=1);
+printf("\n%d",count);
+return 0;
+}
+
